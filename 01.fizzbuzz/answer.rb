4710592ddbb2a8f8c10585dffc0b7612b17e6941
t-yamamoto-being <t-yamamoto@beingcorp.co.jp>
2025-03-03 06:33:46 +0000

    FizzBuzz問題提出2回目

--- 01.fizzbuzz/answer.rb
+++ 01.fizzbuzz/answer.rb
@@ -1,10 +1,9 @@
 (1..20).each do |x|
-  case
-  when x % 3 == 0 && x % 5 == 0
+  if x % 3 == 0 && x % 5 == 0
     puts "FizzBuzz"
-  when x % 3 == 0
+  elsif x % 3 == 0
     puts "Fizz"
-  when x % 5 == 0
+  elsif  x % 5 == 0
     puts "Buzz"
   else
     puts x
