becd112fa772a118cfb779d4f0d580d042fc2d50
t-yamamoto-being <t-yamamoto@beingcorp.co.jp>
2025-03-13 08:37:58 +0000

    ボーリングスコア計算問題_ 提出2回目_sum_scoreメソッドを分割

--- 03.bowling/answer.rb
+++ 03.bowling/answer.rb
@@ -1,50 +1,43 @@
 #!/usr/bin/env ruby
 
-class Bowling
-  attr_accessor :scores
-
+class BowlingScore
   def initialize(score)
-    @scores = score.split(',')
+    @shots = parse_score(score)
   end
 
-  def sum_scores
-    # change to scores
-    shots = []
-    @scores.each do |s|
-      shots << if s == 'X'
-                 10
-               else
-                 s.to_i
-               end
-    end
-
-    # sum total scores
-    total_scores = 0
+  def total_points
+    total_points = 0
     frame_index = 0
     frame_count = 0
 
-    ## process to the 9th frame
     while frame_count < 9
-      if shots[frame_index] == 10 # strike
-        total_scores += shots[frame_index + 1].to_i + shots[frame_index + 2].to_i + 10
+      if @shots[frame_index] == 10 # ストライク
+        total_points += 10 + @shots[frame_index + 1].to_i + @shots[frame_index + 2].to_i
         frame_index += 1
-      elsif shots[frame_index].to_i + shots[frame_index + 1].to_i == 10 # spare
-        total_scores += shots[frame_index + 2].to_i + 10
+      elsif @shots[frame_index].to_i + @shots[frame_index + 1].to_i == 10 # スペア
+        total_points += 10 + @shots[frame_index + 2].to_i
         frame_index += 2
       else
-        total_scores += shots[frame_index].to_i + shots[frame_index + 1].to_i
+        total_points += @shots[frame_index].to_i + @shots[frame_index + 1].to_i
         frame_index += 2
       end
       frame_count += 1
     end
-    ## process the 10th frame
-    total_scores += shots[frame_index..].sum.to_i
+    total_points += final_frame_points(frame_index)
 
-    total_scores
+    total_points
+  end
+
+  private
+
+  def parse_score(score)
+    score.split(',').map { |s| s == 'X' ? 10 : s.to_i }
+  end
+
+  def final_frame_points(frame_index)
+    @shots[frame_index..].sum.to_i
   end
 end
 
-
-bowling = Bowling.new(ARGV[0])
-puts bowling.sum_scores
-
+score = ARGV[0]
+puts BowlingScore.new(score).total_points
